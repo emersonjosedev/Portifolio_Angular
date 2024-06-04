588be807143a3ec6d56207f8093ad233909d03ef
emersonjosedev <emersonmenjob@gmail.com>
2024-06-04 11:26:47 -0300

    '0.01'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,42 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BemVindoComponent } from './components/bem-vindo/bem-vindo.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { MatIconModule } from '@angular/material/icon';
+import { ResumoComponent } from './components/resumo/resumo.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    BemVindoComponent,
+    SobreComponent,
+    ResumoComponent,
+    VideosComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatProgressBarModule
+    
+   
+
   ],
   providers: [
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  
+ 
+}
---
+++ src/app/components/sobre/sobre.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-sobre',
+  templateUrl: './sobre.component.html',
+  styleUrl: './sobre.component.scss'
+})
+export class SobreComponent {
+  email: string = 'emersonmenjob@gmail.com'
+
+}
