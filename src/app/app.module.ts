559e392cc76869da6108c8712c305beb41ef9055
emersonjosedev <emersonmenjob@gmail.com>
2024-07-02 11:05:06 -0300

    0.05

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { MenuPortifolioModule } from './modules/menu-portifolio/menu-portifolio.
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModulosExternosModule } from './modules/modulos-externos/modulos-externos.module';
+import { LogosComponent } from './components/logos/logos.component';
 
 
 @NgModule({
-  declarations: [AppComponent,],
+  declarations: [AppComponent, LogosComponent,],
   imports: [AppRoutingModule, MenuPortifolioModule, ModulosExternosModule],
   providers: [],
   bootstrap: [AppComponent],
